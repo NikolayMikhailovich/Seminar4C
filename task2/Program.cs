aeb12c776f9a0f77a5905cc0385982b6b2969742
nikolays <sidorovkolya91@gmail.com>
2023-01-24 21:59:53 +0300

    Задача2

---
+++ task2/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 1: Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+//456 -> 3
+//78 -> 2
+//89126 -> 5
+
+// Задача 1: Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+
+// создаем ф вводв
+int Input(string message)
+{
+    System.Console.WriteLine(message + ">");
+    string inputValue = Console.ReadLine();
+    int result = Convert.ToInt32(inputValue);
+    return result;
+}
+//  
+int CountRank(int number)
+{
+    int count = 0;
+    while (number > 0)
+    {
+        number = number / 10;
+        count = count + 1;
+    }
+    return count;
+}
+int number = Input ("ввод пользователем числа");
+System.Console.WriteLine(CountRank (number));
