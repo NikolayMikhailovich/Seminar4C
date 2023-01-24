3947d6c485e563a13cabc087425caea730d08ddd
nikolays <sidorovkolya91@gmail.com>
2023-01-24 22:00:31 +0300

    Задача3

---
+++ task3/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 2: Напишите программу, которая принимает на вход число N и 
+//выдаёт произведение чисел от 1 до N.
+//4 -> 24 
+//5 -> 120
+
+//  Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.
+// 4 -> 24 
+// 5 -> 120
+
+// создаем ф вводв
+int Input(string message)
+{
+    System.Console.WriteLine(message + ">");
+    string inputValue = Console.ReadLine();
+    int result = Convert.ToInt32(inputValue);
+    return result;
+}
+int Factorial(int number)
+{
+    int Fact = 1;
+   
+    while (number > 0)
+    {
+        Fact = number * Fact;
+        number = number - 1;
+    }
+
+    return Fact;
+}
+int number = Input ("ввод пользователем числа");
+System.Console.WriteLine(Factorial (number));
